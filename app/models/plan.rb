fb1833f7bd1afb84b96f1c538b3ec04821ae7600
Ninutsa <ninutsa.nadirashvili@gmail.com>
2024-07-12 01:53:01 +0400

    Update homepage to render plan descriptions correctly and add accidentally deleted options_for_select method to Plans model back.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,8 @@
 class Plan < ApplicationRecord
   has_many :plan_description_plans
   has_many :plan_descriptions, through: :plan_description_plans
+
+  def self.options_for_select
+    all.map { |plan| ["#{plan.name} - $#{'%.2f' % plan.price}", plan.id] }
+  end
 end
